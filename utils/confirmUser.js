b278b70f9b351b6d68b601b57858c24be2afa306
Deobaba <adexsquare4192@gmail.com>
2022-11-07 09:11:48 +0100

    i made some more changes

--- utils/confirmUser.js
+++ utils/confirmUser.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const User = require("../models/user");
+const User = require("../src/models/user");
 const ExpressError = require("../utils/expressError");
 
 
